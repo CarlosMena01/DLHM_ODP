c56d1903eb290e4268dae9215452cafa81ce1001
CarlosMena01 <cmena@unal.edu.co>
2023-03-16 22:30:11 -0500

    [FEAT] New API consume

--- static/javascripts/mainPage.js
+++ static/javascripts/mainPage.js
@@ -1,18 +1,58 @@
-function watchCircle() {
-  const x = document.getElementById("CX").value;
-  const y = document.getElementById("CY").value;
-  const radio = document.getElementById("Radio").value;
+// Parámetros globales
+var root = "http://localhost:5000";
 
-  const url = new URL("http://localhost:5000/add_circle");
+var circle = "False";
+var fourier = "False";
+var reconstruction = "False";
+var grid = "False";
+
+function updateState() {
+  const url = new URL(root + "/config_state");
+  url.searchParams.set("circle", circle);
+  url.searchParams.set("fourier", fourier);
+  url.searchParams.set("reconstruction", reconstruction);
+  url.searchParams.set("grid", grid);
+  fetch(url);
+}
+
+function submitCoords() {
+  const x = document.getElementById("x").value;
+  const y = document.getElementById("y").value;
+  const radio = document.getElementById("radius").value;
+
+  const url = new URL(root + "/add_circle");
   url.searchParams.set("radio", radio);
   url.searchParams.set("x", x);
   url.searchParams.set("y", y);
 
-  fetch(url)
-    .then((response) => {})
-    .catch((error) => {});
+  fetch(url);
 }
 
-function openReconstruction() {
-  window.location.href = "http://localhost:5000/reconstruction";
+function toggleGrid() {
+  reconstruction = "False";
+  if (grid === "False") {
+    grid = "True";
+  } else {
+    grid = "False";
+  }
+  updateState();
+}
+
+function toggleFFT() {
+  circle = "True";
+  reconstruction = "False";
+  if (fourier === "False") {
+    fourier = "True";
+  } else {
+    fourier = "False";
+  }
+  updateState();
+}
+
+function reconstructionStart() {
+  circle = "False";
+  fourier = "False";
+  grid = "False";
+  reconstruction = "True";
+  updateState();
 }
