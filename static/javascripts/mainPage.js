39d5f3c377b0b1db2f2a18ec21a13986b29f3a1e
CarlosMena01 <cmena@unal.edu.co>
2023-03-25 21:10:04 -0500

    [ADD] Draw circle in canvas

--- static/javascripts/mainPage.js
+++ static/javascripts/mainPage.js
@@ -1,3 +1,59 @@
+/*----------------------CANVAS READING-------------- */
+const canvas = document.getElementById("mainCanvas");
+const ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+let centerX;
+let centerY;
+// Utiliza la función getCanvasPosition para obtener la posición absoluta del canvas
+let canvasPosition = getCanvasPosition(canvas);
+
+function getCanvasPosition(canvas) {
+  var left = 0;
+  var top = 0;
+  var element = canvas;
+
+  while (element) {
+    left += element.offsetLeft;
+    top += element.offsetTop;
+    element = element.offsetParent;
+  }
+
+  return { x: left, y: top };
+}
+
+canvas.addEventListener("mousedown", function (event) {
+  isDrawing = true;
+
+  // Corrige la posición del mouse para tener en cuenta la posición absoluta del canvas
+  centerX = event.clientX - canvasPosition.x;
+  centerY = event.clientY - canvasPosition.y;
+});
+
+canvas.addEventListener("mousemove", function (event) {
+  if (isDrawing) {
+    console.log(centerX);
+    const radiusX = Math.abs(event.clientX - canvasPosition.x - centerX);
+    const radiusY = Math.abs(event.clientY - canvasPosition.y - centerY);
+    const radius = Math.min(radiusX, radiusY);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCircle(centerX, centerY, radius);
+  }
+});
+
+canvas.addEventListener("mouseup", function (event) {
+  isDrawing = false;
+});
+
+function drawCircle(centerX, centerY, radius) {
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+  ctx.lineWidth = 6;
+  ctx.strokeStyle = "orange";
+  ctx.stroke();
+}
+
+/* ----------------------API INTERACTION------------ */
 // Estado de la aplicación
 const state = {
   circle: false,
