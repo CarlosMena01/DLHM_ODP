0a7c950ee44e616733a713d6e0bb2d0d972c9f0a
CarlosMena01 <cmena@unal.edu.co>
2023-05-17 03:24:25 +0100

    [FEAT] Cv2 camera in threads

--- camera.py
+++ camera.py
@@ -1,14 +1,34 @@
 # Librerias para procesamiento de imagenes
 import cv2
+import numpy as np
+import threading
+import time
 # Clase para abstraer el uso de la cámara
 class Camera:
+    def __start_config(self):
+        self.stop = True
+        time.sleep(1)
+    def __end_config(self):
+        self.stop = False
+        self.thread = threading.Thread(target=self.generate_frame)
+        self.thread.start()
     def __init__(self, source=0, width=640, height=480, exposure_time = 50):
         self.source = source
         self.width = width
         self.height = height
         self.exposure_time = exposure_time
         self.capture = None
+        self.image = np.zeros((self.height, self.width, 3), dtype = np.uint8)
+        self.stop = False
+        self.image_captured = False
+        self.succes = True
 
+    def generate_frame(self):
+        while True:
+            self.succes, self.image = self.capture.read()
+            self.image_captured = True
+            if self.stop:
+                break
     def open(self):
         self.capture = cv2.VideoCapture(self.source)
         # Verificar si la cámara se ha abierto correctamente
@@ -19,13 +39,20 @@ class Camera:
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         self.capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75) # Se desactiva el modo automatico
         self.capture.set(cv2.CAP_PROP_EXPOSURE, self.exposure_time)
+        self.succes, self.image = self.capture.read()
+        time.sleep(2)
+        self.thread = threading.Thread(target=self.generate_frame)
+        self.thread.start()
+        time.sleep(2)
 
     def close(self):
         self.capture.release()
 
     def read(self):
-        succes, image = self.capture.read()
-        return (succes, image)
+        while( not (self.image_captured)):
+            time.sleep(0.001)
+        self.image_captured = False
+        return self.succes, self.image
 
     def set_width(self, width):
         self.width = width
@@ -52,6 +79,7 @@ class Camera:
     
     def config(self, cameraConfig):
         if(cameraConfig["flag"]):
+            self.__start_config()
             # Se configura el tamaño de la cámara
             self.set_height(cameraConfig["height"])
             self.set_width(cameraConfig["width"])
@@ -61,5 +89,6 @@ class Camera:
             
             # Evitamos que se vuelva a configurar cada que se entre
             cameraConfig["flag"] = False
+            self.__end_config()
             return True
         return False
